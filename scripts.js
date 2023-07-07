f7664d0a51c7e686ecf89bb797f603aba2c79611
ElettaLiride <giovanni.costantini92@gmail.com>
2023-07-07 09:10:50 +0200

    changing plan, following tutorial

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-function toggleSidebar() {
-  const sidebar = document.getElementById("sidebar");
-  sidebar.classList.toggle("active");
+/* Set the width of the side navigation to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
 }
